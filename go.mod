23cc7753625de894e1984729386164e6e5e8e397
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2025-01-13 22:48:09 +0700

    create start and help command

--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
-module github.com/mdayat/movies-telegram-bot
+module github.com/mdayat/running-man
 
 go 1.23.4
 
 require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
 	github.com/joho/godotenv v1.5.1
 	github.com/rs/zerolog v1.33.0
 )
--- main.go
+++ main.go
@@ -4,10 +4,24 @@ import (
 	"path/filepath"
 	"strconv"
 
-	"github.com/mdayat/movies-telegram-bot/configs/env"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/mdayat/running-man/configs/env"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
+
+var DefaultMessage = `
+Selamat datang di @RunningManSeriesBot! Dengan bot ini, Anda dapat mengelola dan membeli episode lama dari "Running Man." Berikut adalah perintah yang dapat Anda gunakan:
+
+/start - Selamat datang dan pengenalan
+/help - Dapatkan bantuan dan daftar perintah yang tersedia
+/browse - Tampilkan katalog episode
+/search [nomor_episode] - Cari episode tertentu
+/buy [nomor_episode] - Beli episode
+/checkout - Selesaikan pembelian dengan Telegram stars
+/mycollection - Tampilkan episode yang sudah dibeli
+/feedback - Kirimkan umpan balik atau laporkan masalah
+`
 
 func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
@@ -19,5 +33,39 @@ func main() {
 	err := env.Init()
 	if err != nil {
 		logger.Fatal().Err(err).Send()
+	}
+
+	bot, err := tgbotapi.NewBotAPI(env.BOT_TOKEN)
+	if err != nil {
+		logger.Fatal().Err(err).Send()
+	}
+
+	updateConfig := tgbotapi.NewUpdate(0)
+	updateConfig.Timeout = 60
+	updates := bot.GetUpdatesChan(updateConfig)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		if !update.Message.IsCommand() {
+			continue
+		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+		switch update.Message.Command() {
+		case "start":
+			msg.Text = DefaultMessage
+		case "help":
+			msg.Text = DefaultMessage
+		default:
+			msg.Text = DefaultMessage
+		}
+
+		if _, err := bot.Send(msg); err != nil {
+			logger.Err(err).Msg("failed to send message")
+			continue
+		}
 	}
 }
