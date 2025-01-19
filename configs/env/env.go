5b7500b59be7dad68ef1aea437c0fcdc7289fe2d
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2025-01-19 22:17:48 +0700

    add env variables for running man years and its direct embed base url

--- configs/env/env.go
+++ configs/env/env.go
@@ -7,8 +7,13 @@ import (
 )
 
 var (
-	BOT_TOKEN    string
-	DATABASE_URL string
+	BOT_TOKEN             string
+	DATABASE_URL          string
+	TOKEN_AUTH_KEY_2015   string
+	TOKEN_AUTH_KEY_2018   string
+	TOKEN_AUTH_KEY_2020   string
+	TOKEN_AUTH_KEY_2021   string
+	DIRECT_EMBED_BASE_URL string
 )
 
 func New() error {
@@ -19,6 +24,11 @@ func New() error {
 
 	BOT_TOKEN = os.Getenv("BOT_TOKEN")
 	DATABASE_URL = os.Getenv("DATABASE_URL")
+	TOKEN_AUTH_KEY_2015 = os.Getenv("TOKEN_AUTH_KEY_2015")
+	TOKEN_AUTH_KEY_2018 = os.Getenv("TOKEN_AUTH_KEY_2018")
+	TOKEN_AUTH_KEY_2020 = os.Getenv("TOKEN_AUTH_KEY_2020")
+	TOKEN_AUTH_KEY_2021 = os.Getenv("TOKEN_AUTH_KEY_2021")
+	DIRECT_EMBED_BASE_URL = os.Getenv("DIRECT_EMBED_BASE_URL")
 
 	return nil
 }
