496dc60f342f4c8603aa4ee4a54304dd2df1a848
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2025-02-03 01:02:49 +0700

    add tripay env variables

--- configs/env/env.go
+++ configs/env/env.go
@@ -11,6 +11,9 @@ var (
 	DatabaseURL        string
 	DirectEmbedBaseURL string
 	SupportNumber      string
+	TripayMerchantCode string
+	TripayAPIKey       string
+	TripayPrivateKey   string
 )
 
 func Load() error {
@@ -23,6 +26,9 @@ func Load() error {
 	DatabaseURL = os.Getenv("DATABASE_URL")
 	DirectEmbedBaseURL = os.Getenv("DIRECT_EMBED_BASE_URL")
 	SupportNumber = os.Getenv("SUPPORT_NUMBER")
+	TripayMerchantCode = os.Getenv("TRIPAY_MERCHANT_CODE")
+	TripayAPIKey = os.Getenv("TRIPAY_API_KEY")
+	TripayPrivateKey = os.Getenv("TRIPAY_PRIVATE_KEY")
 
 	return nil
 }
